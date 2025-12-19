fb2813f063864aae92a363d0091b73f2b60515da
Escbrk <69437294+Escbrk@users.noreply.github.com>
2025-12-19 09:13:25 +0000

    Deploying to gh-pages from @ Escbrk/EnergyFlow@30349fb8469aa75ef9fe4a09525ac0d831184470 🚀

--- favorites_page.js
+++ favorites_page.js
@@ -1,2 +1,2 @@
-import{r as c,d,e as n,f as i}from"./assets/modal-DAWlCa-1.js";import"./assets/vendor-CLTxG5yw.js";c.favList.addEventListener("click",t=>{const e=t.target.closest(".exercise-info").dataset.id,s=t.target.closest(".startBtn"),r=t.target.closest(".deleteBtn");if(s){const a=t.currentTarget.dataset.type;d(e,a)}r&&(n(e),i())});
+import{r as c,d,e as n,f as i}from"./assets/modal-kzpZIIlx.js";import"./assets/vendor-CLTxG5yw.js";c.favList.addEventListener("click",t=>{const e=t.target.closest(".exercise-info").dataset.id,s=t.target.closest(".startBtn"),r=t.target.closest(".deleteBtn");if(s){const a=t.currentTarget.dataset.type;d(e,a)}r&&(n(e),i())});
 //# sourceMappingURL=favorites_page.js.map
