b93a92c3c11af50bc775ef23dd0a6ba8f59c59d9
Escbrk <69437294+Escbrk@users.noreply.github.com>
2025-12-25 08:50:10 +0000

    Deploying to gh-pages from @ Escbrk/EnergyFlow@a9f0abd130ca82cc3b22b507db9914750f912257 🚀

---
+++ assets/modal-CIVicbG7.js
@@ -0,0 +1,159 @@
+import{a as m,i as u}from"./vendor-CLTxG5yw.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const a of document.querySelectorAll('link[rel="modulepreload"]'))o(a);new MutationObserver(a=>{for(const r of a)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&o(n)}).observe(document,{childList:!0,subtree:!0});function s(a){const r={};return a.integrity&&(r.integrity=a.integrity),a.referrerPolicy&&(r.referrerPolicy=a.referrerPolicy),a.crossOrigin==="use-credentials"?r.credentials="include":a.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function o(a){if(a.ep)return;a.ep=!0;const r=s(a);fetch(a.href,r)}})();const i={mobileMenu:document.querySelector(".mobile-menu"),mobileMenuBtn:document.querySelector(".mobile-menu-btn"),closeMobileMenuBtn:document.querySelector(".close-btn"),exerciseList:document.querySelector(".exercise-list"),categoryContainer:document.querySelector(".category-container"),exerciseSearchForm:document.querySelector(".exercise-search-form"),backdrop:document.querySelector(".backdrop"),pagination:document.querySelector(".pagination"),favList:document.querySelector(".fav-list"),emptyWrapper:document.querySelector(".empty-wrapper"),quoteWrapper:document.querySelector(".quote-wrapper")},k=location.pathname.split("/").pop()||"index.html";document.querySelectorAll(".header-menu-link, .mobile-nav-link").forEach(e=>{const t=e.getAttribute("href").split("/").pop();k.endsWith(t)&&e.classList.add("active")});i.mobileMenuBtn.addEventListener("click",()=>{i.mobileMenu.classList.remove("hidden")});i.closeMobileMenuBtn.addEventListener("click",()=>{i.mobileMenu.classList.add("hidden")});m.defaults.baseURL="https://energyflow.b.goit.study/api/";const E=async()=>{const{data:e}=await m.get("quote");return e};let S;const N=async(e="Muscles",t=1)=>{S=window.innerWidth<=767?8:12;const{data:s}=await m.get(`filters?filter=${e}&page=${t}&limit=${S}`);return s},P=async(e,t,s=1,o="")=>{S=window.innerWidth<=1439?8:9;const a=e.includes(" ")?"bodypart":e,{data:r}=await m.get(`exercises?${a.toLowerCase()}=${t}&keyword=${o}&page=${s}&limit=${S}`);return r},B=async e=>{const{data:t}=await m.get(`exercises/${e}`);return t},M=async(e,t)=>await m.patch(`exercises/${e}/rating`,t),W=()=>'<span class="loader"></span>',T=()=>'<span class="quote-loader"></span>',$=document.getElementById("quote"),F=async()=>{const e=new Date().toISOString().slice(0,10),t=localStorage.getItem("DailyQuote");let s=t?JSON.parse(t):null;const o={text:$.querySelector(".quote-text"),author:$.querySelector(".author-name")},a=!s||e!==s.date;if(i.quoteWrapper.innerHTML=T(),a)try{const{author:r,quote:n}=await E();s={author:r,quote:n,date:e},localStorage.setItem("DailyQuote",JSON.stringify(s))}catch{u.error({title:"Error",message:"Failed to fetch Daily Quote"}),u.info({title:"Info!",message:"Default quote was setting up!"}),s={author:"EnergyFlow",quote:"Stay active"}}o.text&&o.author&&(i.quoteWrapper.innerHTML=`
+    <p class="quote-text">${s.quote}</p>
+    <p class="author-name">${s.author}</p>
+    `)};F();const q={query:"",category:"",searchTarget:"",data:null},y=e=>e?e[0].toUpperCase()+e.slice(1):"",c="/EnergyFlow/assets/sprite-BaYBotVs.svg",x=()=>{const e=JSON.parse(localStorage.getItem("Favorites"))||[],t=e.map(({name:s,bodyPart:o,burnedCalories:a,time:r,target:n,_id:g})=>`
+            <li class="exercise-info" data-id=${g}>
+              <button class="startBtn" type="button" >Start
+                <svg class="exercise-arrow-icon">
+                  <use href="${c}#icon-arrow"></use>
+                </svg>
+              </button>
+             <div class="wrapp">
+                <p class="info-label">Workout</p>
+                 <button type="button" class="deleteBtn">
+                    <svg class="info-delete">
+                      <use href="${c}#icon-trash"></use>
+                    </svg>
+                 </button>
+             </div>
+                <div class="info-wrapper">
+                  <svg class="exercise-icon">
+                    <use href="${c}#icon-man"></use>
+                  </svg>
+                  <h2 class="info-title">${s}</h2>
+                </div>
+                  <ul class="info-list">
+                    <li class="info-item">Burned calories: <span>${a} / ${r} min</span></li>
+                    <li class="info-item">Body part: <span>${o}</span></li>
+                    <li class="info-item">Target: <span>${n}</span></li>
+                  </ul>
+            </li>
+    `).join("");i.favList&&(i.favList.innerHTML=t,i.emptyWrapper.style.display=e.length===0?"flex":"none")};x();const I=e=>{const s=JSON.parse(localStorage.getItem("Favorites")).filter(({_id:o})=>o!==e);localStorage.setItem("Favorites",JSON.stringify(s)),u.success({title:"Succes",message:"Succesfully deleted from your favorite list"})},O=e=>`
+        <div class="rating-window" data-id=${e}>
+          <button type="button" class="close-modal-btn" data-modal-type="rating">
+            <svg>
+              <use href="${c}#icon-close"></use>
+            </svg>
+          </button>
+          <p class="rating-title">Rating</p>
+
+          <form class="ratingForm">
+            <div class="rating-score">
+              <p class="rating-count">0.0</p>
+
+              <div class="radio-elements">
+                  <label class="rating-star-label">
+                  <input type="radio" name="rate" value="1" required class="visually-hidden">
+                    <svg class="rating-icon">
+                      <use href="${c}#icon-star">1</use>
+                    </svg>
+                  </label>
+                  <label class="rating-star-label">
+                  <input type="radio" name="rate" value="2" required class="visually-hidden">
+                    <svg class="rating-icon">
+                      <use href="${c}#icon-star">1</use>
+                    </svg>
+                  </label>
+                  <label class="rating-star-label">
+                  <input type="radio" name="rate" value="3" required class="visually-hidden">
+                    <svg class="rating-icon">
+                      <use href="${c}#icon-star">1</use>
+                    </svg>
+                  </label>
+                  <label class="rating-star-label">
+                  <input type="radio" name="rate" value="4" required class="visually-hidden">
+                    <svg class="rating-icon">
+                      <use href="${c}#icon-star">1</use>
+                    </svg>
+                  </label>
+                  <label class="rating-star-label">
+                  <input type="radio" name="rate" value="5" required class="visually-hidden">
+                    <svg class="rating-icon">
+                      <use href="${c}#icon-star">1</use>
+                    </svg>
+                  </label>
+              </div>
+            </div>
+            <label for="email"></label>
+            <input type="email" name="email" id="email" class="rating-email-input" placeholder="Email" required/>
+
+            <label for="message"></label>
+            <textarea name="message" id="message" class="rating-message-input" placeholder="Your comment"></textarea>
+
+            <button type="submit" class="ratingSubmitBtn">Send</button>
+          </form>
+        </div>
+    `,w=()=>{i.backdrop.querySelector(".exercise").classList.remove("hidden"),i.backdrop.querySelector(".rating").innerHTML=""},h=()=>{i.backdrop.classList.add("hidden"),document.body.classList.remove("noScroll"),i.backdrop.querySelector(".exercise").innerHTML="",i.backdrop.querySelector(".rating").innerHTML=""};i.backdrop.addEventListener("click",async e=>{var b,v;const t=e.target.closest(".close-modal-btn"),s=e.target.closest(".favorite-btn"),o=e.target.closest(".rating-btn"),a=e.target.closest(".rating-star-label"),r=e.target.closest(".ratingSubmitBtn");let n=(b=e.target.closest(".modal-window"))==null?void 0:b.dataset.id;const g=JSON.parse(localStorage.getItem("Favorites"))||[];if(s)switch(s.dataset.action){case"add":if(g.some(({_id:f})=>f===q.data._id)){u.warning({title:"Warning",message:"Already exist in your favorite list"});return}g.push(q.data),localStorage.setItem("Favorites",JSON.stringify(g)),u.success({title:"Succes",message:"Succesfully added to your favorite list"});break;case"delete":const l=e.target.closest(".modal-window").dataset.id;I(l),x(),h();break}if(t||e.currentTarget.classList.contains("backdrop")){const d=t==null?void 0:t.dataset.modalType;(d==="rating"||e.target.classList.contains("rating"))&&w(),(d==="exercise"||e.target.classList.contains("exercise"))&&h()}if(o&&(i.backdrop.querySelector(".exercise").classList.add("hidden"),i.backdrop.querySelector(".rating").innerHTML=O(n)),r){r.disabled=!0,n=(v=e.target.closest(".rating-window"))==null?void 0:v.dataset.id,e.preventDefault();const d=e.target.closest(".ratingForm"),p=new FormData(d);p.delete("message");const l=Object.fromEntries(p);l.rate=Number(l.rate);try{await M(n,l),w(),h(),u.success({title:"Succes",message:"Rating was succesfully updated!"})}catch(f){u.error({title:"Error",message:f.response.data.message})}finally{r.disabled=!1}}if(a){if(e.target.tagName==="INPUT")return;const d=a.querySelector("input").value;document.querySelector(".rating-count").innerHTML=d;const p=[...document.querySelectorAll(".rating-icon")];p.forEach(l=>l.style.fill="#1b1b1b33");for(let l=0;l<d;l++)p[l].style.fill="#eea10c"}});document.body.addEventListener("keydown",e=>{!i.backdrop.classList.contains("hidden")&&e.code==="Escape"&&(document.querySelector(".rating-window")?w():h())});u.settings({zindex:999999,position:"bottomRight"});const H=async(e,t)=>{try{q.data=await B(e);const{name:s,bodyPart:o,target:a,rating:r,equipment:n,popularity:g,burnedCalories:b,description:v,gifUrl:d,_id:p}=q.data,l=String(r.toFixed(1)),f=t==="available"?`
+            <button type="button" class="favorite-btn" data-action="add">
+              Add to favorites
+              <svg>
+                <use href="${c}#icon-heart"></use>
+              </svg>
+            </button>`:`<button type="button" class="favorite-btn" data-action="delete">
+              Remove
+              <svg>
+                <use href="${c}#icon-heart-broken"></use>
+              </svg>
+            </button>`,L=`
+    <div class="modal-window" data-id="${p}" >
+      <button type="button" class="close-modal-btn" data-modal-type="exercise">
+        <svg>
+          <use href="${c}#icon-close"></use>
+        </svg>
+      </button>
+
+      <div class="gif-wrapper">
+        <img
+          src=${d}
+          alt=${y(s)}
+          class="modal-gif"
+        />
+      </div>
+      <div class="text-wrapper">
+        <h2 class="modal-title">${y(s)}</h2>
+        <p class="modal-rating">
+          ${l}
+          <svg>
+            <use href="${c}#icon-star"></use>
+          </svg>
+        </p>
+
+        <ul class="stats-list">
+          <li class="stats-item">
+            Target
+            <span>${y(a)}</span>
+          </li>
+          <li class="stats-item">
+            Body Part
+            <span>${y(o)}</span>
+          </li>
+          <li class="stats-item">
+            Equipment
+            <span>${y(n)}</span>
+          </li>
+          <li class="stats-item">
+            Popularity
+            <span>${g}</span>
+          </li>
+          <li class="stats-item">
+            Burned Calories
+            <span>${b}/3 min</span>
+          </li>
+        </ul>
+
+        <p class="modal-description">
+        ${v}
+        </p>
+
+        <ul class="btns-list">
+          <li>
+          ${f}
+          </li>
+          <li>
+            <button type="button" class="rating-btn">Give a rating</button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  `;i.backdrop.querySelector(".exercise").innerHTML=L,i.backdrop.classList.remove("hidden"),document.body.classList.add("noScroll")}catch{u.error({title:"Error",message:"Failed to fetch current exercise"}),setTimeout(()=>{i.backdrop.classList.add("hidden")},0)}};export{N as a,P as b,y as c,H as d,I as e,x as f,q as g,W as l,i as r,c as s};
+//# sourceMappingURL=modal-CIVicbG7.js.map
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import{r,g as i,l as y,a as B,c as p,b as C,s as f,d as k}from"./assets/modal-DT07hYYd.js";import{i as x}from"./assets/vendor-CLTxG5yw.js";const g=(e=0,t=0,s=3)=>{const n=Math.floor(s/2);let a=e-n,l=e+n;a<1&&(a=1,l=s),l>t&&(l=t,a=t-s+1),a<1&&(a=1);const d=[];for(let c=a;c<=l;c++)d.push(c);const o=[];//! Left "..."
-if(a>1){o.push(1);//! First page
+import{r as a,g as i,l as y,a as B,c as p,b as C,s as f,d as k}from"./assets/modal-CIVicbG7.js";import{i as x}from"./assets/vendor-CLTxG5yw.js";const g=(e=0,t=0,s=3)=>{const n=Math.floor(s/2);let r=e-n,l=e+n;r<1&&(r=1,l=s),l>t&&(l=t,r=t-s+1),r<1&&(r=1);const d=[];for(let c=r;c<=l;c++)d.push(c);const o=[];//! Left "..."
+if(r>1){o.push(1);//! First page
 o.push("...");//! Spacer
 }//! Main pages
 o.push(...d);//! Right "..."
@@ -7,13 +7,13 @@ if(l<t){o.push("...");//! Spacer
 o.push(t);//! Last page
 }const u=o.map(c=>`<li class="pages_list-item">
                 <button type="button" class="pages_list-btn">${c}</button>
-            </li>`).join("");r.pagination.innerHTML=u,[...document.querySelectorAll(".pages_list-btn")].forEach(c=>{c.textContent.trim()==="..."&&c.classList.add("disabled")})};r.pagination.addEventListener("click",e=>{if(e.target.classList.contains("pages_list-btn")){const t=parseFloat(e.target.textContent);document.querySelector(".exercise-info")?m({category:i.category,query:i.query,currentPage:t,searchTarget:i.searchTarget}):h(i.query,t)}});const L=e=>{[...document.querySelectorAll(".pages_list-btn")].forEach(s=>{const n=parseFloat(s.textContent),a=parseFloat(e);n===a?(s.classList.add("active"),s.disabled=!0):(s.classList.remove("active"),s.disabled=!1)})},h=async(e,t)=>{r.exerciseList.innerHTML=`<li>${y()}</li>`;try{const{results:s,totalPages:n,page:a}=await B(e,t),l=s.map(({imgUrl:d,name:o,filter:u})=>`
+            </li>`).join("");a.pagination.innerHTML=u,[...document.querySelectorAll(".pages_list-btn")].forEach(c=>{c.textContent.trim()==="..."&&c.classList.add("disabled")})};a.pagination.addEventListener("click",e=>{if(e.target.classList.contains("pages_list-btn")){const t=parseFloat(e.target.textContent);document.querySelector(".exercise-info")?m({category:i.category,query:i.query,currentPage:t,searchTarget:i.searchTarget}):h(i.query,t)}});const L=e=>{[...document.querySelectorAll(".pages_list-btn")].forEach(s=>{const n=parseFloat(s.textContent),r=parseFloat(e);n===r?(s.classList.add("active"),s.disabled=!0):(s.classList.remove("active"),s.disabled=!1)})},h=async(e,t)=>{a.exerciseSearchForm.classList.add("hidden"),a.exerciseList.innerHTML=`<li>${y()}</li>`;try{const{results:s,totalPages:n,page:r}=await B(e,t),l=s.map(({imgUrl:d,name:o,filter:u})=>`
           <li class="exercise-item" style="--img: url(${d})">
             <h3 class="exercise-subtitle">${p(o)}</h3>
             <p class="exercise-name">${u}</p>
           </li>
-          `).join("");r.exerciseList.innerHTML=l,r.categoryContainer.innerHTML="",r.exerciseSearchForm.classList.add("hidden"),document.querySelector(".search-input").value="",g(a,n),L(a)}catch({message:s}){x.error({title:"Error",message:`Failet to fetch exercises: ${s}`})}},m=async({category:e,query:t,currentPage:s,searchTarget:n})=>{r.exerciseList.innerHTML=`<li>${y()}</li>`;try{const{results:a,totalPages:l,page:d}=await C(e,t,s,n);let o=a.map(({target:c,rating:$,burnedCalories:q,bodyPart:E,time:F,name:S,_id:T})=>{const w=String($.toFixed(1));return`
-            <li class="exercise-info" data-id=${T}>
+          `).join("");a.exerciseList.innerHTML=l,a.categoryContainer.innerHTML="",document.querySelector(".search-input").value="",g(r,n),L(r)}catch({message:s}){a.exerciseList.innerHTML="",x.error({title:"Error",message:`Failet to fetch exercises: ${s}`})}},m=async({category:e,query:t,currentPage:s,searchTarget:n})=>{a.exerciseList.innerHTML=`<li>${y()}</li>`;try{const{results:r,totalPages:l,page:d}=await C(e,t,s,n);let o=r.map(({target:c,rating:$,burnedCalories:q,bodyPart:E,time:T,name:F,_id:S})=>{const w=String($.toFixed(1));return`
+            <li class="exercise-info" data-id=${S}>
               <button class="startBtn" type="button">Start
                 <svg class="exercise-arrow-icon">
                   <use href="${f}#icon-arrow"></use>
@@ -29,13 +29,13 @@ o.push(t);//! Last page
                   <svg class="exercise-icon">
                     <use href="${f}#icon-man"></use>
                   </svg>
-                  <h2 class="info-title">${S}</h2>
+                  <h2 class="info-title">${F}</h2>
                 </div>
                   <ul class="info-list">
-                    <li class="info-item">Burned calories: <span>${q} / ${F} min</span></li>
+                    <li class="info-item">Burned calories: <span>${q} / ${T} min</span></li>
                     <li class="info-item">Body part: <span>${p(E)}</span></li>
                     <li class="info-item">Target: <span>${p(c)}</span></li>
                   </ul>
             </li>
-    `}).join("");r.categoryContainer.innerHTML=`<span class="exercise-category">${p(i.query)}</span>`;let u;return o.trim()===""?(u=!0,o='<h3 class="unsucces-title">Unfortunately, <span>no results</span> were found. You may want to consider other search options to find the exercise you are looking for. Our range is wide and you have the opportunity to find more options that suit your needs.</h3>'):u=!1,r.exerciseList.innerHTML=o,u?g():g(d,l),L(d),u}catch(a){console.log(a),x.error({title:"Error",message:"Failed to fetch information about exercises"})}},b=document.querySelector(".filter-list"),v=[...b.querySelectorAll("button[data-filter]")];b.addEventListener("click",e=>{const t=e.target.dataset.filter;i.query=e.target.textContent.trim(),t&&(v.forEach(s=>{s.classList.remove("active"),s.disabled=!1}),e.target.classList.add("active"),e.target.disabled=!0,g(),h(i.query))});h();r.exerciseList.addEventListener("click",async e=>{const t=e.target.closest(".exercise-item");if(t&&(i.query=t.children[0].textContent.toLowerCase(),i.category=t.children[1].textContent,v.forEach(n=>{n.disabled=!1}),g(),await m({category:i.category,query:i.query}),r.exerciseSearchForm.classList.remove("hidden")),e.target.closest(".startBtn")){const n=e.target.closest(".exercise-info").dataset.id,a=e.currentTarget.dataset.type;await k(n,a),r.backdrop.classList.remove("hidden"),document.body.classList.add("noScroll")}});r.exerciseSearchForm.addEventListener("submit",async e=>{e.preventDefault();const t=new FormData(r.exerciseSearchForm);i.searchTarget=Object.fromEntries(t).search.toLowerCase(),g(),await m({category:i.category,query:i.query,searchTarget:i.searchTarget})||r.exerciseSearchForm.reset()});
+    `}).join("");a.categoryContainer.innerHTML=`<span class="exercise-category">${p(i.query)}</span>`;let u;return o.trim()===""?(u=!0,o='<h3 class="unsucces-title">Unfortunately, <span>no results</span> were found. You may want to consider other search options to find the exercise you are looking for. Our range is wide and you have the opportunity to find more options that suit your needs.</h3>'):u=!1,a.exerciseList.innerHTML=o,u?g():g(d,l),L(d),u}catch(r){console.log(r),x.error({title:"Error",message:"Failed to fetch information about exercises"})}},b=document.querySelector(".filter-list"),v=[...b.querySelectorAll("button[data-filter]")];b.addEventListener("click",e=>{const t=e.target.dataset.filter;i.query=e.target.textContent.trim(),t&&(v.forEach(s=>{s.classList.remove("active"),s.disabled=!1}),e.target.classList.add("active"),e.target.disabled=!0,g(),h(i.query))});h();a.exerciseList.addEventListener("click",async e=>{const t=e.target.closest(".exercise-item");if(t&&(i.query=t.children[0].textContent.toLowerCase(),i.category=t.children[1].textContent,v.forEach(n=>{n.disabled=!1}),g(),await m({category:i.category,query:i.query}),a.exerciseSearchForm.classList.remove("hidden")),e.target.closest(".startBtn")){const n=e.target.closest(".exercise-info").dataset.id,r=e.currentTarget.dataset.type;await k(n,r),a.backdrop.classList.remove("hidden"),document.body.classList.add("noScroll")}});a.exerciseSearchForm.addEventListener("submit",async e=>{e.preventDefault();const t=new FormData(a.exerciseSearchForm);i.searchTarget=Object.fromEntries(t).search.toLowerCase(),g(),await m({category:i.category,query:i.query,searchTarget:i.searchTarget})||a.exerciseSearchForm.reset()});
 //# sourceMappingURL=index.js.map
