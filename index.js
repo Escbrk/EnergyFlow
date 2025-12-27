0f0ea3e2b30d2e8276d51f4da52e686742b4d9e1
Escbrk <69437294+Escbrk@users.noreply.github.com>
2025-12-27 11:24:20 +0000

    Deploying to gh-pages from @ Escbrk/EnergyFlow@c6bf3c4389649a4ff1fb4c15dfff7bc5bf7ed455 🚀

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import{r as a,g as i,l as y,a as B,c as p,b as C,s as f,d as k}from"./assets/modal-D1YaIpC-.js";import{i as x}from"./assets/vendor-CLTxG5yw.js";const g=(e=0,t=0,s=3)=>{const n=Math.floor(s/2);let r=e-n,l=e+n;r<1&&(r=1,l=s),l>t&&(l=t,r=t-s+1),r<1&&(r=1);const d=[];for(let c=r;c<=l;c++)d.push(c);const o=[];//! Left "..."
+import{r as a,g as i,l as x,a as B,c as f,b as C,s as m,d as k,e as M}from"./assets/modal-BuHZdhKo.js";import{i as p}from"./assets/vendor-CLTxG5yw.js";const g=(e=0,t=0,s=3)=>{const n=Math.floor(s/2);let r=e-n,l=e+n;r<1&&(r=1,l=s),l>t&&(l=t,r=t-s+1),r<1&&(r=1);const d=[];for(let c=r;c<=l;c++)d.push(c);const o=[];//! Left "..."
 if(r>1){o.push(1);//! First page
 o.push("...");//! Spacer
 }//! Main pages
@@ -7,35 +7,35 @@ if(l<t){o.push("...");//! Spacer
 o.push(t);//! Last page
 }const u=o.map(c=>`<li class="pages_list-item">
                 <button type="button" class="pages_list-btn">${c}</button>
-            </li>`).join("");a.pagination.innerHTML=u,[...document.querySelectorAll(".pages_list-btn")].forEach(c=>{c.textContent.trim()==="..."&&c.classList.add("disabled")})};a.pagination.addEventListener("click",e=>{if(e.target.classList.contains("pages_list-btn")){const t=parseFloat(e.target.textContent);document.querySelector(".exercise-info")?m({category:i.category,query:i.query,currentPage:t,searchTarget:i.searchTarget}):h(i.query,t)}});const L=e=>{[...document.querySelectorAll(".pages_list-btn")].forEach(s=>{const n=parseFloat(s.textContent),r=parseFloat(e);n===r?(s.classList.add("active"),s.disabled=!0):(s.classList.remove("active"),s.disabled=!1)})},h=async(e,t)=>{a.exerciseSearchForm.classList.add("hidden"),a.exerciseList.innerHTML=`<li>${y()}</li>`;try{const{results:s,totalPages:n,page:r}=await B(e,t),l=s.map(({imgUrl:d,name:o,filter:u})=>`
+            </li>`).join("");a.pagination.innerHTML=u,[...document.querySelectorAll(".pages_list-btn")].forEach(c=>{c.textContent.trim()==="..."&&c.classList.add("disabled")})};a.pagination.addEventListener("click",e=>{if(e.target.classList.contains("pages_list-btn")){const t=parseFloat(e.target.textContent);document.querySelector(".exercise-info")?y({category:i.category,query:i.query,currentPage:t,searchTarget:i.searchTarget}):h(i.query,t)}});const L=e=>{[...document.querySelectorAll(".pages_list-btn")].forEach(s=>{const n=parseFloat(s.textContent),r=parseFloat(e);n===r?(s.classList.add("active"),s.disabled=!0):(s.classList.remove("active"),s.disabled=!1)})},h=async(e,t)=>{a.exerciseSearchForm.classList.add("hidden"),a.exerciseList.innerHTML=`<li>${x()}</li>`;try{const{results:s,totalPages:n,page:r}=await B(e,t),l=s.map(({imgUrl:d,name:o,filter:u})=>`
           <li class="exercise-item" style="--img: url(${d})">
-            <h3 class="exercise-subtitle">${p(o)}</h3>
+            <h3 class="exercise-subtitle">${f(o)}</h3>
             <p class="exercise-name">${u}</p>
           </li>
-          `).join("");a.exerciseList.innerHTML=l,a.categoryContainer.innerHTML="",document.querySelector(".search-input").value="",g(r,n),L(r)}catch({message:s}){a.exerciseList.innerHTML="",x.error({title:"Error",message:`Failet to fetch exercises: ${s}`})}},m=async({category:e,query:t,currentPage:s,searchTarget:n})=>{a.exerciseList.innerHTML=`<li>${y()}</li>`;try{const{results:r,totalPages:l,page:d}=await C(e,t,s,n);let o=r.map(({target:c,rating:$,burnedCalories:q,bodyPart:E,time:T,name:F,_id:S})=>{const w=String($.toFixed(1));return`
-            <li class="exercise-info" data-id=${S}>
+          `).join("");a.exerciseList.innerHTML=l,a.categoryContainer.innerHTML="",document.querySelector(".search-input").value="",g(r,n),L(r)}catch({message:s}){a.exerciseList.innerHTML="",p.error({title:"Error",message:`Failet to fetch exercises: ${s}`})}},y=async({category:e,query:t,currentPage:s,searchTarget:n})=>{a.exerciseList.innerHTML=`<li>${x()}</li>`;try{const{results:r,totalPages:l,page:d}=await C(e,t,s,n);let o=r.map(({target:c,rating:E,burnedCalories:$,bodyPart:q,time:F,name:T,_id:w})=>{const S=String(E.toFixed(1));return`
+            <li class="exercise-info" data-id=${w}>
               <button class="startBtn" type="button">Start
                 <svg class="exercise-arrow-icon">
-                  <use href="${f}#icon-arrow"></use>
+                  <use href="${m}#icon-arrow"></use>
                 </svg>
               </button>
               <p class="info-label">Workout</p>
-              <span class="info-ranking">${w}
+              <span class="info-ranking">${S}
                 <svg>
-                  <use href="${f}#icon-star"></use>
+                  <use href="${m}#icon-star"></use>
                 </svg>
               </span>
                 <div class="info-wrapper">
                   <svg class="exercise-icon">
-                    <use href="${f}#icon-man"></use>
+                    <use href="${m}#icon-man"></use>
                   </svg>
-                  <h2 class="info-title">${F}</h2>
+                  <h2 class="info-title">${T}</h2>
                 </div>
                   <ul class="info-list">
-                    <li class="info-item">Burned calories: <span>${q} / ${T} min</span></li>
-                    <li class="info-item">Body part: <span>${p(E)}</span></li>
-                    <li class="info-item">Target: <span>${p(c)}</span></li>
+                    <li class="info-item">Burned calories: <span>${$} / ${F} min</span></li>
+                    <li class="info-item">Body part: <span>${f(q)}</span></li>
+                    <li class="info-item">Target: <span>${f(c)}</span></li>
                   </ul>
             </li>
-    `}).join("");a.categoryContainer.innerHTML=`<span class="exercise-category">${p(i.query)}</span>`;let u;return o.trim()===""?(u=!0,o='<h3 class="unsucces-title">Unfortunately, <span>no results</span> were found. You may want to consider other search options to find the exercise you are looking for. Our range is wide and you have the opportunity to find more options that suit your needs.</h3>'):u=!1,a.exerciseList.innerHTML=o,u?g():g(d,l),L(d),u}catch(r){console.log(r),x.error({title:"Error",message:"Failed to fetch information about exercises"})}},b=document.querySelector(".filter-list"),v=[...b.querySelectorAll("button[data-filter]")];b.addEventListener("click",e=>{const t=e.target.dataset.filter;i.query=e.target.textContent.trim(),t&&(v.forEach(s=>{s.classList.remove("active"),s.disabled=!1}),e.target.classList.add("active"),e.target.disabled=!0,g(),h(i.query))});h();a.exerciseList.addEventListener("click",async e=>{const t=e.target.closest(".exercise-item");if(t&&(i.query=t.children[0].textContent.toLowerCase(),i.category=t.children[1].textContent,v.forEach(n=>{n.disabled=!1}),g(),await m({category:i.category,query:i.query}),a.exerciseSearchForm.classList.remove("hidden")),e.target.closest(".startBtn")){const n=e.target.closest(".exercise-info").dataset.id,r=e.currentTarget.dataset.type;await k(n,r),a.backdrop.classList.remove("hidden"),document.body.classList.add("noScroll")}});a.exerciseSearchForm.addEventListener("submit",async e=>{e.preventDefault();const t=new FormData(a.exerciseSearchForm);i.searchTarget=Object.fromEntries(t).search.toLowerCase(),g(),await m({category:i.category,query:i.query,searchTarget:i.searchTarget})||a.exerciseSearchForm.reset()});
+    `}).join("");a.categoryContainer.innerHTML=`<span class="exercise-category">${f(i.query)}</span>`;let u;return o.trim()===""?(u=!0,o='<h3 class="unsucces-title">Unfortunately, <span>no results</span> were found. You may want to consider other search options to find the exercise you are looking for. Our range is wide and you have the opportunity to find more options that suit your needs.</h3>'):u=!1,a.exerciseList.innerHTML=o,u?g():g(d,l),L(d),u}catch(r){console.log(r),p.error({title:"Error",message:"Failed to fetch information about exercises"})}},b=document.querySelector(".filter-list"),v=[...b.querySelectorAll("button[data-filter]")];b.addEventListener("click",e=>{const t=e.target.dataset.filter;i.query=e.target.textContent.trim(),t&&(v.forEach(s=>{s.classList.remove("active"),s.disabled=!1}),e.target.classList.add("active"),e.target.disabled=!0,g(),h(i.query))});h();a.exerciseList.addEventListener("click",async e=>{const t=e.target.closest(".exercise-item");if(t&&(i.query=t.children[0].textContent.toLowerCase(),i.category=t.children[1].textContent,v.forEach(n=>{n.disabled=!1}),g(),await y({category:i.category,query:i.query}),a.exerciseSearchForm.classList.remove("hidden")),e.target.closest(".startBtn")){const n=e.target.closest(".exercise-info").dataset.id,r=e.currentTarget.dataset.type;await k(n,r),a.backdrop.classList.remove("hidden"),document.body.classList.add("noScroll")}});a.exerciseSearchForm.addEventListener("submit",async e=>{e.preventDefault();const t=new FormData(a.exerciseSearchForm);i.searchTarget=Object.fromEntries(t).search.toLowerCase(),g(),await y({category:i.category,query:i.query,searchTarget:i.searchTarget})||a.exerciseSearchForm.reset()});a.footerForm.addEventListener("submit",async e=>{e.preventDefault();const t=e.currentTarget,s=new FormData(t),n=Object.fromEntries(s);try{const r=await M(n);p.success({title:"Succes",message:r.data.message}),t.reset()}catch(r){p.error({title:"Error",message:r.response.data.message})}});
 //# sourceMappingURL=index.js.map
