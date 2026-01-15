b6bb2d83d5f19ef84a57bc500949a16c34769a98
Paul <69437294+Escbrk@users.noreply.github.com>
2026-01-15 01:48:31 -0700

    upd

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,4 @@ import './header.js';
 import './quote.js';
 import './render.js';
 import './subscriptionForm.js';
-import './handlers/scrollToTopHandler.js'
+import './handlers/scrollToTopHandler.js';
